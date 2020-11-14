1dde8b99dfa77279dbbc08d23ef0ea1b1da16ee5
Guilherme B <38567020+gui1080@users.noreply.github.com>
2020-11-14 13:05:28 -0800

    comentários feitos

--- código/CamadaEnlace.cpp
+++ código/CamadaEnlace.cpp
@@ -18,6 +18,7 @@ using namespace std;
 // https://github.com/gui1080/TR1_Trabalho_Camada_de_Enlace
 // (repositório privado na entrega do trabalho)
 
+// TRANSMISSÃO
 //-----------------------------------------------------
 
 void CamadaEnlaceDadosTransmissora(int quadro[])
@@ -47,11 +48,13 @@ int *CamadadeEnlaceTransmissoraEnquadramento(int quadro[])
 
   cout << "\033[1;35mCamada Enlace Transmissora ENQUADRAMENTO\n \033[0m\n";
 
+  // se escolhe o tipo de enquadramento
   int tipoDeEnquadramento = ENQUADRAMENTO;
 
   int *quadroEnquadrado;
   int i, size;
 
+  // se enquadra o fluxo de bits passado
   switch (tipoDeEnquadramento)
   {
   case 0:
@@ -88,6 +91,7 @@ int *CamadadeEnlaceTransmissoraEnquadramento(int quadro[])
     break;
   }
 
+  // retornamos o fluxo de bits enquadrado
   return quadroEnquadrado;
 }
 
@@ -114,7 +118,7 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBits(int quadro[])
 
   int flag[8] = {0, 1, 1, 1, 1, 1, 1, 0};
 
-  int divisao = ((size / 40));
+  int divisao = ((size / 40)); // quantidade de grupos inteiros que se forma com o numero de bits passado
   int resto = ((size % 40) / 8); // qntd de bytes que tem q entrar
 
   /*
@@ -142,7 +146,7 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBits(int quadro[])
   }
   else
   {
-    quantidade_total_quadros = (size / 40) + 1;
+    quantidade_total_quadros = (size / 40) + 1; // o mais 1 indica que não teremos um quadro com o tamanho inteiro como os outros
   }
 
   printf("tamanho do fluxo: %d / resto: %d\n / quantidade de coiso: %d / divisao: %d\n", tamanho_fluxo, resto, quantidade_total_quadros, divisao);
@@ -152,6 +156,8 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBits(int quadro[])
   tamanho_fluxo = size + tam_bits_adicionais;
   fluxoCodificado = new (nothrow) int[tamanho_fluxo];
 
+  // o vetor auxiliar irá indicar a quantidade de bits que entra no grupo que se está passando
+  // do fluxo não enquadrado para o fluxo enquadrado
   int *vetor_aux;
   vetor_aux = new (nothrow) int[quantidade_total_quadros];
 
@@ -190,6 +196,7 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBits(int quadro[])
   while (i < size)
   {
 
+    // copia a flag
     for (x = 0; x < 8; x++)
     {
       fluxoCodificado[i_aux + x] = flag[x];
@@ -197,13 +204,14 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBits(int quadro[])
 
     i_aux = i_aux + 8;
 
+    // se aloca a quantidade de bits certa dependendo do que o vetor auxiliar indica
     for (y = 0; y < (vetor_aux[k] * 8); y++)
     {
 
       if( y == 4){
       if ((quadro[i + y] == 1) && (quadro[i + y - 1] == 1) && (quadro[i + y - 2] == 1) && (quadro[i + y - 3] == 1) && (quadro[i + y - 4] == 1))
         {
-                  
+          // caso ocorra uma sequencia grande de 1's, se adiciona um zero        
           fluxoCodificado[i_aux + y] = 0;
           i_aux++;
         }
@@ -211,10 +219,12 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBits(int quadro[])
       fluxoCodificado[i_aux + y] = quadro[i + y];
     }
 
+    // atualizamos de acordo as variaveis que irão iterar por cada vetor
     cont_um = 0;
     i = i + (vetor_aux[k] * 8);
     i_aux = i_aux + (vetor_aux[k] * 8);
 
+    // coloca a flag denovo delimitando este grupo
     for (x = 0; x < 8; x++)
     {
 
@@ -245,6 +255,8 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBits(int quadro[])
   // 01111110 0011111 0 10011111 0 10011111 0 10011111 0 10011111 0 1 01111110
   //          0011111   10011111   10011111   10011111   10011111   1
 
+
+  // se passa adiante o fluxo com o tamanho correto
 
   fluxoCodificado[i_aux] = 2;
 
@@ -283,6 +295,9 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBytes(int quadro[])
 
   int resto = ((size % 40) / 8); // qntd de bytes que tem q entrar
 
+  // se aloca o novo tamanho do bloco de mensagem dependendo da
+  // quantidade de grupos que se aloca em um tamanho certo entre as flags
+
   if (size < 40)
   {
 
@@ -314,6 +329,9 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBytes(int quadro[])
   int *fluxoCodificado;
   fluxoCodificado = new (nothrow) int[tamanho_fluxo];
 
+
+  // o vetor auxiliar irá indicar a quantidade de bits que entra no grupo que se está passando
+  // do fluxo não enquadrado para o fluxo enquadrado
   int *vetor_aux;
   vetor_aux = new (nothrow) int[quantidade_total_quadros];
 
@@ -346,6 +364,7 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBytes(int quadro[])
   while (i < tamanho_fluxo)
   {
 
+    // se coloca a flag no começo do grupo de bits
     for (x = 0; x < 8; x++)
     {
 
@@ -354,15 +373,20 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBytes(int quadro[])
 
     i = i + 8;
 
+    // se coloca a quantidade correta de bits dentro do fluxo enquadrado
+    // de acordo com o vetor auxiliar
     for (y = 0; y < (vetor_aux[k] * 8); y++)
     {
 
       fluxoCodificado[i + y] = quadro[i_aux + y];
+    
     }
 
+    // atualizamos de acordo as variaveis que irão iterar por cada vetor
     i = i + (vetor_aux[k] * 8);
     i_aux = i_aux + (vetor_aux[k] * 8);
 
+    // se bota a flag novamente delimitando tamanho
     for (x = 0; x < 8; x++)
     {
 
@@ -383,6 +407,8 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBytes(int quadro[])
   }
 
   printf("\n\n");
+
+  // se determina o final e passa adiante o fluxo enquadrado
 
   fluxoCodificado[tamanho_fluxo] = 2;
 
@@ -433,6 +459,9 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoContagemDeCaracteres(int quadro[])
 
   int quantidade_total_quadros;
 
+  // se aloca a quantidade total de quadros para se determinar quantos grupos de tamanho especifico podem ser criados
+  // para que então se faça um grupo com o resto
+
   if (resto == 0)
   {
     quantidade_total_quadros = (size / 24);
@@ -441,6 +470,9 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoContagemDeCaracteres(int quadro[])
   {
     quantidade_total_quadros = (size / 24) + 1;
   }
+
+  // o vetor auxiliar irá indicar a quantidade de bits que entra no grupo que se está passando
+  // do fluxo não enquadrado para o fluxo enquadrado
 
   int *aux;
   aux = new (nothrow) int[quantidade_total_quadros];
@@ -472,6 +504,9 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoContagemDeCaracteres(int quadro[])
   x = 0;
   while (i < tamanho_fluxo)
   {
+
+    // se coloca a "flag", nesse caso
+    // o tamanho do bloco que se esta colocando em um quadro
 
     if (x == 0)
     {
@@ -508,6 +543,9 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoContagemDeCaracteres(int quadro[])
       x = 1;
       i = i + 8;
     }
+
+    // dependendo do tamanho, se coloca a quantidade de bits certa na mensagem
+
     if (x == 1)
     {
 
@@ -516,8 +554,9 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoContagemDeCaracteres(int quadro[])
 
         fluxoCodificado[cont + i] = quadro[cont + i_aux];
       }
+      // atualizamos de acordo as variaveis que irão iterar por cada vetor
       i = i + (aux[k] * 8);
-      x = 0;
+      x = 0; // a variavel x acaba fazendo com que os dois ifs neste loop se alternem
       i_aux = i_aux + (aux[k] * 8);
     }
 
@@ -535,6 +574,8 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoContagemDeCaracteres(int quadro[])
     printf("%d", fluxoCodificado[x]);
   }
 
+  // se delimita o final e passa o fluxo correto adiante
+
   fluxoCodificado[tamanho_fluxo] = 2;
 
   //int *fluxoCodificado;
@@ -544,6 +585,7 @@ int *CamadaDeEnlaceTransmissoraEnquadramentoContagemDeCaracteres(int quadro[])
   return fluxoCodificado;
 }
 
+// RECEPÇÃO
 //-----------------------------------------------------
 
 void CamadaEnlaceDadosReceptora(int quadro[])
@@ -591,43 +633,46 @@ int *CamadaDeEnlaceReceptoraEnquadramento(int quadro[])
 
   printf("\n\nrecepção, enlace\n\n");
 
+  // aqui se escolhe o tipo de enquadramento que se deseja retirar
   int tipoDeEnquadramento = ENQUADRAMENTO;
 
-  int *quadroEnquadrado;
+  int *quadroDesenquadrado;
 
   int i = find_size(quadro);
 
-  quadroEnquadrado = new (nothrow) int[i];
+  quadroDesenquadrado = new (nothrow) int[i];
   int *quadroFinal;
 
+  // desenquadramos de acordo com a abordagem escolhida
   switch (tipoDeEnquadramento)
   {
   case 0:
 
     printf("passeando pela recepção enlace :)");
 
-    quadroEnquadrado = CamadaDeEnlaceReceptoraEnquadramentoContagemDeCaracteres(quadro);
+    quadroDesenquadrado = CamadaDeEnlaceReceptoraEnquadramentoContagemDeCaracteres(quadro);
 
     break;
 
   case 1:
 
-    quadroEnquadrado = CamadaDeEnlaceReceptoraEnquadramentoInsercaoDeBytes(quadro);
+    quadroDesenquadrado = CamadaDeEnlaceReceptoraEnquadramentoInsercaoDeBytes(quadro);
     //
     break;
 
   case 2:
 
-    quadroEnquadrado = CamadaDeEnlaceReceptoraEnquadramentoInsercaoDeBits(quadro);
+    quadroDesenquadrado = CamadaDeEnlaceReceptoraEnquadramentoInsercaoDeBits(quadro);
     //
-    int l = find_size(quadroEnquadrado); 
+    int l = find_size(quadroDesenquadrado); 
 
     printf("\n\n velho: %d novo: %d\n\n", i, l); 
 
     break;
   }
 
-  return quadroEnquadrado;
+  // passa o quadro pronto adiante
+  return quadroDesenquadrado;
 }
 
 // Desenquadramentos
@@ -659,7 +704,8 @@ int *CamadaDeEnlaceReceptoraEnquadramentoInsercaoDeBits(int quadro[])
 
   while (i < size)
   {
-
+    
+    // se analisa de 8 em 8 bits atrás da flag
     for (x = 0; x < 8; x++)
     {
 
@@ -675,6 +721,7 @@ int *CamadaDeEnlaceReceptoraEnquadramentoInsercaoDeBits(int quadro[])
     else
     {
 
+      // se busca sequencias de 1, para que o zero da sequencia seja descartado
       if (quadro[i] == 1 && quadro[i - 1] == 1)
       {
         cont_um++;
@@ -691,13 +738,15 @@ int *CamadaDeEnlaceReceptoraEnquadramentoInsercaoDeBits(int quadro[])
 
       FluxoFinal[i_aux] = quadro[i];
 
+      // copia o que é mensagem para o fluxo, 
+      // jogando flags fora
       for (x = 0; x < 8; x++)
       {
 
         FluxoFinal[x + i_aux] = aux[x];
       }
 
-
+      // atualizamos de acordo as variaveis que irão iterar por cada vetor
       i_aux = i_aux + 8;
     }
 
@@ -725,6 +774,7 @@ int *CamadaDeEnlaceReceptoraEnquadramentoInsercaoDeBits(int quadro[])
   
   printf("\n");
 
+  // passa o resultado adiante
   return FluxoFinal;
 }
 
@@ -745,12 +795,14 @@ int *CamadaDeEnlaceReceptoraEnquadramentoInsercaoDeBytes(int quadro[])
   while (i < size)
   {
 
+    // se faz a análise de 8 em 8 bits
     for (x = 0; x < 8; x++)
     {
 
       aux[x] = quadro[x + i];
     }
 
+    // buscamos a flag
     if ((aux[0] == 0) && (aux[1] == 0) && (aux[2] == 0) && (aux[3] == 0) && (aux[4] == 1) && (aux[5] == 1) && (aux[6] == 1) && (aux[7] == 1))
     {
 
@@ -758,12 +810,14 @@ int *CamadaDeEnlaceReceptoraEnquadramentoInsercaoDeBytes(int quadro[])
     }
     else
     {
-
+      // se não é a flag, podemos copiar os bits para o resultado final
       for (x = 0; x < 8; x++)
       {
 
         FluxoFinal[x + i_aux] = aux[x];
       }
+
+      // atualizamos de acordo as variaveis que irão iterar por cada vetor
 
       i_aux = i_aux + 8;
     }
@@ -777,6 +831,8 @@ int *CamadaDeEnlaceReceptoraEnquadramentoInsercaoDeBytes(int quadro[])
     printf("%d", FluxoFinal[x]);
   }
   printf("\n\n");
+
+  // delimitamos um final e passamos o fluxo adiante
 
   FluxoFinal[size] = 2;
 
@@ -872,6 +928,8 @@ int *CamadaDeEnlaceReceptoraEnquadramentoContagemDeCaracteres(int quadro[])
   }
 
   printf("\n\n");
+
+  // delimitamos o final e passamos o fluxo adiante
 
   FluxoFinal[i_aux] = 2;
 
